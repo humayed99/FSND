2f4f534d2f841eab4ee33eb65e8b6de6d8d73443
Sara Humayed <humayed99@Saras-MacBook-Pro.local>
2021-07-03 19:31:15 +0300

    I'm still working on it and adding commands

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,4 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgres://humayed99@localhost:5432/fyyurtest'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
